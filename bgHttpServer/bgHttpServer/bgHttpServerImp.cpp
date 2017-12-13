cd4d20b40b4885baa9080e9d752bd8eedb015766
baggiowangyu <baggiowangyu@gmail.com>
2017-12-14 04:04:29 +0800

    增加HP-Socket依赖头文件；开始重新设计HTTP服务器

--- bgHttpServer/bgHttpServer/bgHttpServerImp.cpp
+++ bgHttpServer/bgHttpServer/bgHttpServerImp.cpp
@@ -6,16 +6,17 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
-
-//////////////////////////////////////////////////////////////////////////
-//
-//
-//
-//////////////////////////////////////////////////////////////////////////
+#define USE_DEFAULT_SSL_SETTTING
+#ifdef USE_DEFAULT_SSL_SETTTING
+#define CA_CERT		_T("bgCA.crt")		// CA根证书
+#define SRV_CERT	_T("bgServer.cer")	// 服务器用的SSL公钥证书
+#define SRV_PRI		_T("bgServer.key")	// 服务器用的SSL私钥证书（pkm？）
+#define SRV_PIN		_T("123456")		// 服务器用的SSL私钥证书密钥
+#endif
 
 bgHttpServerImp::bgHttpServerImp()
 {
-
+	
 }
 
 bgHttpServerImp::~bgHttpServerImp()
@@ -26,6 +27,18 @@ bgHttpServerImp::~bgHttpServerImp()
 int bgHttpServerImp::OnInit()
 {
 	int errCode = 0;
+
+	// 前期准备工作，读取配置文件
+
+	// 创建HTTP服务监听器对象
+	http_server_listener_ = ::Create_HP_HttpServerListener();
+
+	// 创建HTTP服务器对象
+	http_server_ = ::Create_HP_HttpServer(http_server_listener_);
+	https_server_ = ::Create_HP_HttpsServer(http_server_listener_);
+
+	// 初始化SSL环境参数，测试版里面暂时把各种证书、密钥等信息写死
+	BOOL bret = ::HP_SSLServer_SetupSSLContext(https_server_, SSL_VM_NONE, SRV_CERT, SRV_PRI, SRV_PIN, CA_CERT, SIN_ServerNameCallback);
 
 	return errCode;
 }
--- bgHttpServer/bgHttpServer/bgHttpServerImp.h
+++ bgHttpServer/bgHttpServer/bgHttpServerImp.h
@@ -4,21 +4,9 @@
 #define _HTTP_SUPPORT
 #define _SSL_SUPPORT
 
-#include "HttpServer.h"
-#include "Global/helper.h"
-
 #include <atlstr.h>
 
-class CHttpServerListenerImpl : public CHttpServerListener
-{
-private:
-
-public:
-	CHttpServerListenerImpl()
-	{
-	}
-};
-
+#include "HPSocket4C-SSL.h"
 
 class bgHttpServerImp
 {
@@ -27,14 +15,40 @@ public:
 	~bgHttpServerImp();
 
 public:
+	static En_HP_HandleResult __stdcall OnPrepareListen(HP_Server pSender, SOCKET soListen);
+	static En_HP_HandleResult __stdcall OnAccept(HP_Server pSender, HP_CONNID dwConnID, SOCKET soClient);
+	static En_HP_HandleResult __stdcall OnHandShake(HP_Server pSender, HP_CONNID dwConnID);
+	static En_HP_HandleResult __stdcall OnReceive(HP_Server pSender, HP_CONNID dwConnID, const BYTE* pData, int iLength);
+	static En_HP_HandleResult __stdcall OnSend(HP_Server pSender, HP_CONNID dwConnID, const BYTE* pData, int iLength);
+	static En_HP_HandleResult __stdcall OnClose(HP_Server pSender, HP_CONNID dwConnID, EnSocketOperation enOperation, int iErrorCode);
+	static En_HP_HandleResult __stdcall OnShutdown(HP_Server pSender);
+
+	static En_HP_HttpParseResult __stdcall OnMessageBegin(HP_HttpServer pSender, HP_CONNID dwConnID);
+	static En_HP_HttpParseResult __stdcall OnRequestLine(HP_HttpServer pSender, HP_CONNID dwConnID, LPCSTR lpszMethod, LPCSTR lpszUrl);
+	static En_HP_HttpParseResult __stdcall OnHeader(HP_HttpServer pSender, HP_CONNID dwConnID, LPCSTR lpszName, LPCSTR lpszValue);
+	static En_HP_HttpParseResult __stdcall OnHeadersComplete(HP_HttpServer pSender, HP_CONNID dwConnID);
+	static En_HP_HttpParseResult __stdcall OnBody(HP_HttpServer pSender, HP_CONNID dwConnID, const BYTE* pData, int iLength);
+	static En_HP_HttpParseResult __stdcall OnChunkHeader(HP_HttpServer pSender, HP_CONNID dwConnID, int iLength);
+	static En_HP_HttpParseResult __stdcall OnChunkComplete(HP_HttpServer pSender, HP_CONNID dwConnID);
+	static En_HP_HttpParseResult __stdcall OnMessageComplete(HP_HttpServer pSender, HP_CONNID dwConnID);
+	static En_HP_HttpParseResult __stdcall OnUpgrade(HP_HttpServer pSender, HP_CONNID dwConnID, EnHttpUpgradeType enUpgradeType);
+	static En_HP_HttpParseResult __stdcall OnParseError(HP_HttpServer pSender, HP_CONNID dwConnID, int iErrorCode, LPCSTR lpszErrorDesc);
+
+	static En_HP_HandleResult __stdcall OnWSMessageHeader(HP_HttpServer pSender, HP_CONNID dwConnID, BOOL bFinal, BYTE iReserved, BYTE iOperationCode, const BYTE lpszMask[4], ULONGLONG ullBodyLen);
+	static En_HP_HandleResult __stdcall OnWSMessageBody(HP_HttpServer pSender, HP_CONNID dwConnID, const BYTE* pData, int iLength);
+	static En_HP_HandleResult __stdcall OnWSMessageComplete(HP_HttpServer pSender, HP_CONNID dwConnID);
+
+public:
+	static int CALLBACK SIN_ServerNameCallback(LPCTSTR lpszServerName);
+
+public:
 	int OnInit();
 
 public:
-	CHttpServerListenerImpl m_HttpListener;
-	CHttpServerListenerImpl m_HttpsListener;
+	HP_HttpServer http_server_;
+	HP_HttpsServer https_server_;
 
-	CHttpServer m_HttpServer;
-	CHttpsServer m_HttpsServer;
+	HP_HttpServerListener http_server_listener_;
 };
 
 #endif//_BG_HTTP_SERVER_IMP_H_
